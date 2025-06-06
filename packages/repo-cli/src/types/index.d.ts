fca3353037a7ae9198ba4183d317c492a9d26c1b
fermeridamagni <fermeridamagni@outlook.com>
2025-06-05 20:28:47 -0600

    refactor(index.d.ts): restructure FunctionResult type definition for clarity

--- packages/repo-cli/src/types/index.d.ts
+++ packages/repo-cli/src/types/index.d.ts
@@ -24,11 +24,18 @@ export type Commit = DefaultLogFields & ListLogLine;
 /**
  * @description The result type for functions that return a success or failure message.
  */
-export type FunctionResult<T = { [key: string]: string }> = Promise<{
+export type FunctionResult<T = { [key: string]: string }> = {
   success: boolean;
   message: string;
   data?: T; // Optional data returned on success
-}>;
+};
+
+/**
+ * @description The options for a function, which can include additional parameters.
+ */
+export type FunctionResultPromise<T = { [key: string]: string }> = Promise<
+  FunctionResult<T>
+>;
 // #endregion Functions
 
 // #region Single Projects
