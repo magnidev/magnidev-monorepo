7fa75697ec098096a83641c0666e77867edba021
fermeridamagni <fermeridamagni@outlook.com>
2025-06-05 19:04:40 -0600

    refactor(@magnidev/repo-cli): enhance type descriptions and standardize region comments in index.d.ts

--- packages/repo-cli/src/types/index.d.ts
+++ packages/repo-cli/src/types/index.d.ts
@@ -11,57 +11,57 @@ import {
   monorepoProjectRootPackageJsonSchema,
   monorepoProjectPackageJsonSchema,
 } from "@/schemas/providers/monorepoProjectSchemas";
+import { CommandOptions } from "commander";
 
+// #region Git
 /**
- * The configuration tool schema for the repository.
- */
-export type ConfigTool = SingleProjectConfig | MonorepoProjectConfig;
-
-/**
- * A commit object that includes default log fields and a list log line.
+ * @description A commit object that includes default log fields and a list log line.
  */
 export type Commit = DefaultLogFields & ListLogLine;
+// #endregion - Git
 
+// #region Functions
 /**
- * The result type for functions that return a success or failure message.
+ * @description The result type for functions that return a success or failure message.
  */
 export type FunctionResult<T = { [key: string]: string }> = Promise<{
   success: boolean;
   message: string;
   data?: T; // Optional data returned on success
 }>;
+// #endregion Functions
 
 // #region Single Projects
 /**
- * The configuration schema for single projects.
+ * @description The configuration schema for single projects.
  */
 export type SingleProjectConfig = z.infer<typeof singleProjectConfigSchema>;
 
 /**
- * The package.json schema for single projects.
+ * @description The package.json schema for single projects.
  */
 export type SingleProjectPackageJson = z.infer<
   typeof singleProjectPackageJsonSchema
 >;
-// #endregion
+// #endregion Single Projects
 
 // #region Monorepo Projects
 /**
- * The configuration schema for monorepo projects.
+ * @description The configuration schema for monorepo projects.
  */
 export type MonorepoProjectConfig = z.infer<typeof monorepoProjectConfigSchema>;
 
 /**
- * The package.json schema for monorepo root projects.
+ * @description The package.json schema for monorepo root projects.
  */
 export type MonorepoProjectRootPackageJson = z.infer<
   typeof monorepoProjectRootPackageJsonSchema
 >;
 
 /**
- * The package.json schema for monorepo packages.
+ * @description The package.json schema for monorepo packages.
  */
 export type MonorepoProjectPackageJson = z.infer<
   typeof monorepoProjectPackageJsonSchema
 >;
-// #endregion
+// #endregion Monorepo Projects
