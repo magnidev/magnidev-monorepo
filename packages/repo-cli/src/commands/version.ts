938cb12f14ee6029e46d0d747746636681732a4b
fermeridamagni <fermeridamagni@outlook.com>
2025-06-09 17:12:21 -0600

    feat: enhance version command to support package selection in monorepos

--- packages/repo-cli/src/commands/version.ts
+++ packages/repo-cli/src/commands/version.ts
@@ -1,17 +1,89 @@
+import prompts from "@clack/prompts";
 import { Command } from "commander";
+import colors from "picocolors";
 
-type VersionCommandOptions = {
-  tag?: string;
-};
+import RepositoryClient from "@lib/repositoryClient";
+import { intro } from "@/utils/intro";
+import { onCommandFlowCancel, onCommandFlowError } from "@/utils/events";
+
+type VersionCommandOptions = {};
 
 function versionCommand(program: Command): Command {
   return program
     .command("version")
     .description("bump the version")
-    .option("-t, --tag <tag>", "version tag")
     .action(async (options: VersionCommandOptions) => {
       // #region Initialization
-      const { tag } = options;
+      const {} = options;
+
+      prompts.updateSettings({
+        aliases: {
+          w: "up",
+          s: "down",
+          a: "left",
+          d: "right",
+          esc: "cancel",
+        },
+      });
+
+      prompts.intro(colors.white(intro));
+      // #endregion Initialization
+
+      try {
+        // #region - Initialize Clients
+        const repositoryClient = new RepositoryClient();
+        const gitClient = repositoryClient.gitClient;
+
+        // Check if the repository is a monorepo or single project
+        const repoType = await repositoryClient.getRepoType();
+        if (!repoType.success || !repoType.data) {
+          onCommandFlowCancel(repoType.message);
+        }
+        // #endregion - Initialize Clients
+
+        // #region - Command Flow
+        const userConfig = await prompts.group({
+          // #region - @packageName
+          packageName: async () => {
+            if (repoType.data === "single") {
+              const foundPackage =
+                await repositoryClient.singleProjectProvider.getPackage();
+              if (!foundPackage.success || !foundPackage.data) {
+                onCommandFlowCancel(foundPackage.message);
+              }
+
+              return foundPackage.data!.name;
+            }
+
+            if (repoType.data === "monorepo") {
+              const foundPackages =
+                await repositoryClient.monorepoProjectProvider.getPackages();
+              if (!foundPackages.success || !foundPackages.data) {
+                onCommandFlowCancel(foundPackages.message);
+              }
+
+              const options = foundPackages.data!.map((pkg) => ({
+                label: pkg.name,
+                value: pkg.name,
+                hint: pkg.version,
+              }));
+
+              return await prompts.select({
+                message: "Select the package to bump the version:",
+                options,
+                maxItems: 1,
+              });
+            }
+
+            // If we reach here, it means an invalid type was selected
+            onCommandFlowCancel("Invalid repository type.");
+          },
+          // #endregion - @packageName
+        });
+        // #endregion - Command Flow
+      } catch (error: any) {
+        onCommandFlowError(error as Error);
+      }
     });
 }
 
