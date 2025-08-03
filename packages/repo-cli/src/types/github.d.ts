653b3260a74694d19163e2881314a1be09c661bc
fermeridamagni <hello@magni.dev>
2025-08-02 18:51:20 -0600

    deprecate Repo CLI, moved to https://github.com/fermeridamagni/Orbit

--- packages/repo-cli/src/types/github.d.ts
+++
@@ -1,6 +0,0 @@
-import type { DefaultLogFields, ListLogLine } from "simple-git";
-
-/**
- * @description A commit object that includes default log fields and a list log line.
- */
-export type Commit = DefaultLogFields & ListLogLine;
--- packages/repo-cli/src/utils/console.ts
+++
@@ -1,4 +0,0 @@
-import { promisify } from "node:util";
-import { exec } from "node:child_process";
-
-export const execAsync = promisify(exec);
