892fa9bfb74c2956016930b378c815f39c268376
fermeridamagni <hello@magni.dev>
2026-01-04 00:37:04 -0600

    refactor: simplify exports in index file for validation hooks and schemas

--- packages/react-hooks/src/index.ts
+++ packages/react-hooks/src/index.ts
@@ -1,16 +1,4 @@
 // biome-ignore lint/performance/noBarrelFile: This is a library entry point, not a barrel file
-export {
-  useValidateEmail,
-  useValidateName,
-  useValidatePassword,
-  useValidateText,
-  useValidateUsername,
-  type ValidationResult,
-} from "./hooks/use-validations";
+export * from "./hooks/use-validations";
 
-export {
-  emailSchema,
-  nameSchema,
-  passwordSchema,
-  usernameSchema,
-} from "./schemas/auth-schema";
+export * from "./schemas/auth-schema";
